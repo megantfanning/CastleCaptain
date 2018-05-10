16a83aab22b4e23f1931ed2d06f8f02a332ff936
Megan Fanning <mfanning@mac-mfanning.tsi.lan>
2018-05-09 17:26:36 -0700

    adding more CSS magic and clean up captains log

--- src/CaptainsLog.js
+++ src/CaptainsLog.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 
-// NOTE add new items to front rather than end
+//TODO li's probably need keys here?
 function CaptainsLog(props){
-    const intro =[ "Took on Crew at Parnas","Begin Voyage, fair eastward winds",];
-    // const test =["Full moon and thunder tonight", "Lizard worshipers unhappy"];
+    const intro =[ "Landed in Parnas to take on crew","Began Voyage, fair eastward winds",];
     const reverseChronLog = intro.concat(props.log)
     const log = reverseChronLog.reverse();
     const listItems = log.map((log) => <li>{log}</li>);
