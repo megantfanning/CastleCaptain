f2324911dbfb1e3505706b1aa9bf3ef308d7181d
Megan Fanning <mfanning@mac-mfanning.tsi.lan>
2018-05-10 16:17:37 -0700

    clean up CSS

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {NavigationButton, NavigationToggle} from './Navigation';
 import {CaptainsLog} from'./CaptainsLog';
 import './App.css';
 
+const commands= [["Anchor","Drift"],["Spin", "Widdershins"], ["Rise", "Sink"],["North", "South"], ["East", "West"]]
+
 class App extends Component {
   test=["test"];
   render() {
@@ -13,10 +15,10 @@ class App extends Component {
           <h1 className="App-title">Welcome Castle Captain</h1>
           <div class="d-flex m-50 align:center">
             <NavigationToggle navAnimation1="spin" label1="Spin" img={logo} navAnimation2="widdershins" label2="Widdershins"/>
-            <NavigationToggle navAnimation1="icon" label1="Rise" img={logo} navAnimation2="icon" label2="Sink"/>
+            <NavigationToggle navAnimation1="riseIcon" label1="Rise" img={logo} navAnimation2="sinkIcon" label2="Sink"/>
             <NavigationToggle navAnimation1="icon" label1="North" img={logo} navAnimation2="icon" label2="South"/>
             <NavigationToggle navAnimation1="icon" label1="East" img={logo} navAnimation2="icon" label2="West"/>
-            <NavigationToggle navAnimation1="icon" label1="Anchor" img={logo} navAnimation2="icon" label2="Drift"/>
+            <NavigationToggle navAnimation1="icon" label1="Anchor" img={logo} navAnimation2="driftIcon" label2="Drift"/>
           </div>
         </header>
         <div class="sky" >
